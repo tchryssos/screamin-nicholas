4f939b0dc433957c4e42b5937b1272b5d50079b0
Troy Chryssos <troy@codecademy.com>
2021-08-26 23:11:24 -0400

    using dotenv

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,8 @@
 import { Client, Intents } from 'discord.js';
 import dotenv from 'dotenv';
-
 dotenv.config();
-
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
-  console.log('Ready!');
+    console.log('Ready!');
 });
-
-// Login to Discord with your client's token
 client.login(process.env.TOKEN);
