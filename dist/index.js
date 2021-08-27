1f9f7648a562100e7aabf7e12f738f87c9db7b80
Troy Chryssos <troy@codecademy.com>
2021-08-26 23:22:42 -0400

    messing with build commands

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,1 @@
-import { Client, Intents } from 'discord.js';
-import dotenv from 'dotenv';
-dotenv.config();
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.once('ready', () => {
-    console.log('Ready!');
-});
-client.login(process.env.TOKEN);
+import{Client,Intents}from"discord.js";import dotenv from"dotenv";dotenv.config();const client=new Client({intents:[Intents.FLAGS.GUILDS]});client.once("ready",()=>{console.log("Ready!")}),client.login(process.env.TOKEN);
