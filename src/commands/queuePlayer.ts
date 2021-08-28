37e7201d7ccd44565a8f5be0390405874af42356
Troy Chryssos <troychryssos@gmail.com>
2021-08-28 16:39:01 -0400

    no, the validation functions don't work very well since I need to break the fn call if false

---
+++ src/commands/queuePlayer.ts
@@ -0,0 +1,5 @@
+import { CommandInteraction } from 'discord.js';
+
+import { validateIsInServer, validateIsInVoice } from './validations.js';
+
+export const queuePlayer = (interaction: CommandInteraction) => {};
