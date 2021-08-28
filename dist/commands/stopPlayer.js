faed1ef286ba808f9c9d91f1bd4cf5a37049ad52
Troy Chryssos <troychryssos@gmail.com>
2021-08-28 14:53:22 -0400

    removing dist from github, who needs it

--- dist/commands/stopPlayer.js
+++
@@ -1,9 +0,0 @@
-import { currentQueueRef } from '../state/queue.js';
-export const stopPlayer = (interaction) => {
-    const { player } = currentQueueRef;
-    if (player) {
-        player.pause();
-        interaction.reply('Stopping player');
-        currentQueueRef.current = null;
-    }
-};
--- dist/constants/commands.js
+++
@@ -1,2 +0,0 @@
-export const PLAY = 'play';
-export const STOP = 'stop';
