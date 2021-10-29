11ea163a9f53acea7a343858f864b6fe693eff3b
Troy Chryssos <troychryssos@gmail.com>
2021-10-28 22:56:26 -0400

    testing getting spotify playlist data

--- src/commands/utils/fetchAudioData.ts
+++ src/commands/utils/fetchAudioData.ts
@@ -1,10 +1,46 @@
 import { CommandInteraction } from 'discord.js';
+import dotenv from 'dotenv';
+import SpotifyWebApi from 'spotify-web-api-node';
 import ytdl from 'ytdl-core';
 import ytsr, { Video } from 'ytsr';
 
 // eslint-disable-next-line import/extensions
 import { VideoMeta, YTDLStream } from '../../typings/queue';
 import { durationToSeconds } from '../../utils/durationToSeconds.js';
+
+dotenv.config();
+let spotifyOk = false;
+const checkIfSpotifyOkay = () => spotifyOk;
+
+// START - SPOTIFY CONFIG - START
+// https://github.com/thelinmichael/spotify-web-api-node#client-credential-flow
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+});
+
+spotifyApi.clientCredentialsGrant().then((data) => {
+  try {
+    spotifyApi.setAccessToken(data.body['access_token']);
+    // eslint-disable-next-line no-console
+    console.log('Successfully authenticated with Spotify');
+    spotifyOk = true;
+  } catch {
+    console.warn('Something went wrong trying to set the Spotify access token');
+  }
+});
+
+const testPlaylist = async () => {
+  try {
+    const resp = await spotifyApi.getPlaylist(
+      '7GQ0qcoHvGOqbKHVw73q4y?si=108bcf85426c4c07'
+    );
+    console.log(resp.body.tracks.items);
+  } catch (e) {
+    console.log('SPOTIFY ERROR: ', JSON.stringify(e));
+  }
+};
+// END - SPOTIFY CONFIG - END
 
 export const fetchStream = async (youtubeUrl: string) =>
   ytdl(youtubeUrl, {
@@ -68,11 +104,26 @@ export const tryFetchStream = async (
   let stream: ReturnType<typeof ytdl>;
   let url: string;
   let meta: VideoMeta | undefined;
+  await testPlaylist();
   if (ytdl.validateURL(searchText)) {
     stream = await fetchStream(searchText);
     url = searchText;
     meta = undefined;
-  } else {
+  }
+  // else if (searchText.includes('spotify') && checkIfSpotifyOkay()) {
+  //   try {
+  //     const playlist = await spotifyApi.getPlaylist(
+  //       'https://open.spotify.com/playlist/7GQ0qcoHvGOqbKHVw73q4y?si=172749d34d0c4769'
+  //     );
+  //     console.log('spotify: ', JSON.stringify(playlist));
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //   url = '';
+  //   stream = {} as YTDLStream;
+  //   meta = {} as VideoMeta;
+  // }
+  else {
     const { stream: searchedStream, meta: searchedMeta } =
       await fetchYoutubeTopResultStream(searchText);
     url = searchedMeta.url;
