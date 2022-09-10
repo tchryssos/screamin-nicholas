fedf15a205b63e14b5d9ffb7abd758d9f2ed87d5
Troy Chryssos <troy@codecademy.com>
2022-09-10 15:53:36 -0400

    not sure this will stop the 403 from crashing nicholas, but its worth a try

--- src/commands/utils/playAudio.ts
+++ src/commands/utils/playAudio.ts
@@ -21,51 +21,57 @@ export const playAudio = async (
   voiceChannelId: string,
   guild: Guild
 ) => {
-  const initialPlayer = currentQueueRef.player;
-  let player = initialPlayer;
-  if (!player) {
-    player = createAudioPlayer();
-    currentQueueRef.player = player;
-  }
+  try {
+    const initialPlayer = currentQueueRef.player;
+    let player = initialPlayer;
+    if (!player) {
+      player = createAudioPlayer();
+      currentQueueRef.player = player;
+    }
 
-  const resource = createAudioResource(stream, {
-    inputType: StreamType.Arbitrary,
-    inlineVolume: true,
-  });
-
-  currentQueueRef.current.resource = resource;
-
-  // Join voice channel
-  const connection = joinVoiceChannel({
-    channelId: voiceChannelId,
-    guildId: guild.id,
-    adapterCreator: guild!.voiceAdapterCreator as DiscordGatewayAdapterCreator,
-  });
-
-  // Play the audio, and related things
-  player.play(resource);
-  connection.subscribe(player);
-
-  /*
-    Since we can't respond to an interaction twice
-    as in the case of an IDLE status re-firing playAudio with the next in the queue
-    we need to send a normal message to a channel
-  */
-  const nowPlayingMessage = createNowPlayingMessage(
-    currentQueueRef.current.meta?.title || ''
-  );
-
-  await reply(nowPlayingMessage, interaction);
-
-  if (!initialPlayer) {
-    player.on(AudioPlayerStatus.Idle, async () => {
-      if (currentQueueRef.queue.length) {
-        await playNextTrack(interaction, voiceChannelId, guild);
-      } else {
-        connection.destroy();
-        currentQueueRef.player = null;
-        currentQueueRef.current = {};
-      }
+    const resource = createAudioResource(stream, {
+      inputType: StreamType.Arbitrary,
+      inlineVolume: true,
     });
+
+    currentQueueRef.current.resource = resource;
+
+    // Join voice channel
+    const connection = joinVoiceChannel({
+      channelId: voiceChannelId,
+      guildId: guild.id,
+      adapterCreator: guild!
+        .voiceAdapterCreator as DiscordGatewayAdapterCreator,
+    });
+
+    // Play the audio, and related things
+    player.play(resource);
+    connection.subscribe(player);
+
+    /*
+      Since we can't respond to an interaction twice
+      as in the case of an IDLE status re-firing playAudio with the next in the queue
+      we need to send a normal message to a channel
+    */
+    const nowPlayingMessage = createNowPlayingMessage(
+      currentQueueRef.current.meta?.title || ''
+    );
+
+    await reply(nowPlayingMessage, interaction);
+
+    if (!initialPlayer) {
+      player.on(AudioPlayerStatus.Idle, async () => {
+        if (currentQueueRef.queue.length) {
+          await playNextTrack(interaction, voiceChannelId, guild);
+        } else {
+          connection.destroy();
+          currentQueueRef.player = null;
+          currentQueueRef.current = {};
+        }
+      });
+    }
+  } catch (e) {
+    await reply((e as Error).message, interaction);
+    await playNextTrack(interaction, voiceChannelId, guild);
   }
 };
